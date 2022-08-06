4933866eafa471534d6f7efc3b01a75afa515ecf
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-08-06 17:20:41 +0530

    FCFS scheduling algo with gantt chart

--- fcfs.c
+++ fcfs.c
@@ -2,81 +2,90 @@
 #include<stdlib.h>
 
 struct pcb{
-    int pid, at, bt, ct, wt, tt;
-    struct pcb * link;
+    int pid, at, bt, ct, tt, wt;
 };
-int totWT = 0, totTT = 0;
-
-typedef struct pcb * PCB;
-
-PCB create(){
-    PCB new = (PCB)malloc(sizeof(struct pcb));
-    if ( new == NULL){
-        printf("Memory error!!\n");
-        return NULL;
-    }
-    new -> link = NULL;
-    return new;
-}
-
-// ReadyQ is ordering the linked list
-PCB readyQ(PCB ready, PCB new){
-    if ( ready == NULL || new -> at <= ready -> at ){
-        new -> link = ready;
-        return new;
-    }
-        
-    PCB temp = ready -> link;
-    PCB prev = ready;
-    while ( temp != NULL && temp -> at < new -> at ){
-        prev = temp;
-        temp = temp -> link;
-    }
-
-    prev -> link = new;
-    new -> link = temp;
-    return ready;
-}
-
-void calculate(PCB ready){
-    int ct = ready -> at;
-    PCB temp = ready;
-
-    while ( temp != NULL){
-        if ( ct < temp -> at)
-            ct = temp -> at;
-
-        ct += temp -> bt;
-        temp -> ct = ct;
-        temp -> tt = temp-> ct - temp -> at;
-        temp -> wt = temp -> tt - temp -> bt;
-        totTT += temp -> tt;
-        totWT += temp -> wt;
-        temp = temp -> link;
-    }
-}
 
 void main(){
     int n;
-    PCB ready = NULL;
-    PCB new;
-    printf("Enter the number of processes >> ");
+    printf("Enter num of processes >> ");
     scanf("%d", &n);
 
-    printf("Enter PID  AT  BT\n");
-    for(int i = 0; i<n; ++i){
-        new = create();
-        int pid, at, bt;
-        scanf("%d%d%d", &new -> pid, &new -> at, &new -> bt);
-        ready = readyQ(ready, new);
-    }
+    struct pcb p[n];
 
-    calculate(ready);
+    printf("PID  AT  BT\n");
+    for(int i= 0; i<n; ++i)
+        scanf("%d%d%d", &p[i].pid, &p[i].at, &p[i].bt);
+
+    // Initializing completed array to 0
+    int * isCompleted = (int *)calloc(sizeof(int),n);
+
+    int min, index , completed = 0, curtime = 0, pid[20], ct[20], len = n, prev = -1, tottt = 0, totwt = 0, j = 0;
+
+    while ( completed != n ){
+        index = -1, min = 99999;
+
+        // Selection of process of minimum at
+        for(int i = 0; i<n; ++i){
+            if (p[i].at <= curtime && !isCompleted[i]){
+                if ( min > p[i].at){
+                    index = i;
+                    min = p[i].at;
+                }
+            }
+        }
+        
+        if ( index != -1 ){
+            p[index].ct = p[index].bt + curtime;
+            p[index].tt = p[index].ct - p[index].at;
+            p[index].wt = p[index].tt - p[index].bt;
+
+            tottt += p[index].tt;
+            totwt += p[index].wt;
+
+            ct[j] = p[index].ct;
+            pid[j] = p[index].pid;
+            ++j;
+
+            curtime = p[index].ct;
+            ++completed;
+            isCompleted[index] = 1;
+        }
+        else{
+            if (prev == p[index].pid)
+                ++ct[j];
+            else{
+                pid[j] = -1;
+                ++ct[j];
+                ++len;
+                ++j;
+                prev = p[index].pid;
+            }
+            ++curtime;
+        }
+    }
 
     printf("PID\tAT\tBT\tCT\tTT\tWT\n");
-    PCB temp = ready;
-    while(temp != NULL){
-        printf("%d\t%d\t%d\t%d\t%d\t%d\n", temp -> pid, temp -> at, temp -> bt, temp -> ct, temp -> tt, temp -> wt);
-        temp = temp -> link;
+    for(int i = 0; i<n; ++i)
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i].pid, p[i].at, p[i].bt, p[i].ct, p[i].tt, p[i].wt);
+
+    printf("\nGantt Chart >> \n");
+    for(int i = 0; i<len; ++i)
+        printf("-------");
+    printf("\n");
+    for(int i = 0; i<len; ++i){
+        if ( pid[i] != -1 )
+            printf("  P%d  |", pid[i]);
+        else
+            printf(" IDLE |");
     }
+    printf("\n");
+    for(int i = 0; i<len; ++i)
+        printf("-------");
+    printf("\n");
+    printf("0     ");
+    for(int i = 0; i<len; ++i)
+        printf("%d      ", ct[i]);
+
+    printf("\n\nAvg turn around time >> %.2f\n", (float)tottt/n);
+    printf("Avg waiting time >> %.2f\n", (float)totwt/n);
 }
