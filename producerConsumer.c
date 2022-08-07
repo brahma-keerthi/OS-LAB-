f806546910bf0eed882953ddc5cef6fe7c9e2782
Brahma Keerthi <brahmakeerthihs008@gmail.com>
2022-08-07 18:36:45 +0530

    Producer - Consumer using mutex semaphores

--- producerConsumer.c
+++ producerConsumer.c
@@ -1,57 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<semaphore.h>
+#include<pthread.h>
 
-int mutex = 1, full = 0, empty = 3, x = 0;
+sem_t empty, full;
+pthread_mutex_t mutex;
+int in = 0, out = 0;
+int buffer[3];
 
-int Wait (int s){
-    return --s;
+void * producer( void *pno){
+    for(int i = 0; i<3; ++i){
+    sem_wait(&empty);
+    pthread_mutex_lock(&mutex);
+
+    buffer[in] = rand()%1000;
+    printf("PRODUCER - %d : Insert item %d at %d\n", *((int *)pno), buffer[in], in);
+    in = (in + 1)%3;
+
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full);
+    }
 }
 
-int Signal (int s){
-    return ++s;
-}
+void * consumer( void *pno ){
+    for(int i = 0; i<3; ++i){
+    sem_wait(&full);
+    pthread_mutex_lock(&mutex);
 
-void producer(){
-    mutex = Wait(mutex);
-    full = Signal(full);
-    empty = Wait(empty);
-    ++x;
-    printf("PRODUCER -- Produced item %d\n", x);
-    mutex = Signal(mutex);
-}
+    printf("CONSUMER - %d : Remove item %d at %d\n", *(int *)pno, buffer[out] , out);
+    out = (out + 1)%3;
 
-void consumer(){
-    mutex = Wait(mutex);
-    empty = Signal(empty);
-    full = Wait(full);
-    printf("CONSUMER -- Consumed item %d\n", x);
-    --x;
-    mutex = Signal(mutex);
+    pthread_mutex_unlock(&mutex);
+    sem_post(&empty);
+    }
 }
 
 void main(){
-    int ch;
-    while(1){
-        printf("1. To produce\n2. To consume\n");
-        scanf("%d", &ch);
+    pthread_t con[3], pro[3];
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&empty, 0, 3);
+    sem_init(&full, 0, 0);
 
-        switch (ch){
-            case 1:
-                if (mutex == 1 && empty != 0)
-                    producer();
-                else
-                    printf("Buffer is full\n");
-                break;
+    int a[3] = {1, 2, 3};
 
-            case 2:
-                if (mutex == 1 && full != 0)
-                    consumer();
-                else  
-                    printf("Buffer is empty\n");
-                break;
+    for(int i = 0; i<3; ++i)
+        pthread_create(&pro[i], NULL, (void *)producer, (void *)&a[i]);
 
-            default:
-                exit(1);
-        }
-    }
+    for(int i = 0; i<3; ++i)
+        pthread_create(&con[i], NULL, (void *)consumer, (void *)&a[i]);
+
+    for(int i = 0; i<3; ++i)
+        pthread_join(pro[i], NULL);
+    
+    for(int i = 0; i<3; ++i)
+        pthread_join(con[i], NULL);
+
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&empty);
+    sem_destroy(&full);
 }
